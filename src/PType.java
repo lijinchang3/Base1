76d8e9e691ff47ffcd8d61cc7d8395f34cb9acf4
Administrator <lijinchang3@163.com>
2018-11-20 13:52:25 +0800

    11

--- src/PType.java
+++ src/PType.java
@@ -1,17 +1,15 @@
 import java.awt.List;
 import java.util.ArrayList;
 import javax.swing.plaf.metal.MetalIconFactory.FolderIcon16;
-
 public class PType {
 	/**
 	 * @param args
 	 */
-	
-	
-	
 	public static void main(String[] args) {
 		// TODO ×Ô¶¯Éú³ÉµÄ·½·¨´æ¸ù
 		//dsafdsafdsa
+		
+		
 	    ArrayList<Integer> list=new ArrayList<Integer>();
 		int i = 0;
 		for (; i < 10; i++) {
