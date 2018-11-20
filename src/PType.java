ff0d873d12307a71b36768b7d3c305f5fe519ca8
Administrator <lijinchang3@163.com>
2018-11-20 12:53:09 +0800

    1111

---
+++ src/PType.java
@@ -0,0 +1,20 @@
+import java.awt.List;
+import java.util.ArrayList;
+
+import javax.swing.plaf.metal.MetalIconFactory.FolderIcon16;
+
+public class PType {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+	    ArrayList<Integer> list=new ArrayList<Integer>();
+		int i = 0;
+		for (; i < 10; i++) {
+			System.out.println(i);
+			System.out.println(i+2);
+		}
+	}
+
+}
